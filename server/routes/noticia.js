0595bdfac85da1dfe01d55d5b4b091bcf7793d4a
Cl4udits <claudiita.olmos@gmail.com>
2018-08-23 12:44:35 -0300

    Added: ruta de noticia

--- server/routes/noticia.js
+++ server/routes/noticia.js
@@ -4,8 +4,9 @@
 
 'use strict';
 const express = require('express');
+const NoticiaController = require('../controller/noticia');
 
 const api = express.Router();
-
+api.post('/crearNoticia', NoticiaController.crearNoticia);
 
 module.exports = api;
